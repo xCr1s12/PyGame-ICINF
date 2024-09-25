683c5bb106d8c9bd13da3d1442e2f8075e0a2410
xCr1s12 <CristobalAlexis.onate@alumnos.ulagos.cl>
2024-09-24 21:59:48 -0300

    cambio-width

--- EJ 2/main.py
+++ EJ 2/main.py
@@ -12,7 +12,7 @@ pygame.display.set_caption("Alo?")
 
 run = True 
 circulo1 = Circulo(250, 250, 50)
-circulo2 = Circulo2(200, 200, 50)
+circulo2 = Circulo2(100, 100, 50)
 while run:
     screen.fill((0,0,0))
     for event in pygame.event.get():
